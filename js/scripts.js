d29148456b8ef59463e3f167b7d62d19edb5ea56
Blake Scott and James Turello <jturello@yahoo.com>
2016-01-20 13:46:08 -0800

    add all else functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 var piglatin = function(str) {
-
+ /**********************
+ The var firstVowel will find the numeric position of the first vowel.
+ The var firstLetters is specifically for the instance where the first vowel is a u preceded by a q. It slices from the U and everything before it.
+ The var firstConsts slices up to but not including the first vowel.
+ ******************/
   var firstVowel = str.search(/[aeiou].*/);
   var firstLetters = str.slice(0,firstVowel + 1);
+  var firstConsts = str.slice(0,firstVowel);
+
 
   if (str.substring(0,1) === "a") {
     return str.concat("ay");
@@ -21,7 +27,7 @@ var piglatin = function(str) {
       }
     }
 
-    else { return str.slice(firstVowel, str.length).concat(firstLetters, "ay");
+    else { return str.slice(firstVowel, str.length).concat(firstConsts, "ay");
   }
 };
 
