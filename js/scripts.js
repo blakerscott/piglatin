982a0166bcce485483bb4585ac242f8b6a87248e
Blake Scott and James Turello <jturello@yahoo.com>
2016-01-20 10:21:16 -0800

    refactored code by using if else statements instead of switch

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,36 @@
 var piglatin = function(str) {
-    switch(str.substring(0,1)){
-      case "a" : return str.concat("ay"); break;
-      case "e" : return str.concat("ay"); break;
-      case "i" : return str.concat("ay"); break;
-      case "o" : return str.concat("ay"); break;
-      case "u" : return str.concat("ay"); break;
-    }
-  };
+  if (str.substring(0,1) === "a") {
+    return str.concat("ay");
+  } else if (str.substring(0,1) === "e") {
+    return str.concat("ay");
+  } else if (str.substring(0,1) === "i") {
+    return str.concat("ay");
+  } else if (str.substring(0,1) === "o") {
+    return str.concat("ay");
+  } else if (str.substring(0,1) === "u") {
+    return str.concat("ay");
+  }
+    else if 
+
+};
+
+
+
+
+
+
+
+
+
+//
+// This is an alternate way to write the if else statement
+//  1. "string".substring(0, "string".search(/[aeiou].*/));
+//
+//  2.   switch(str.substring(0,1)){
+//     //   case "a" : return str.concat("ay"); break;
+//     //   case "e" : return str.concat("ay"); break;
+//     //   case "i" : return str.concat("ay"); break;
+//     //   case "o" : return str.concat("ay"); break;
+//     //   case "u" : return str.concat("ay"); break;
+//     // }
+//     // if str
--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,10 @@
 describe('piglatin', function() {
   it("will add 'ay' to the end if the word starts with a vowel", function() {
-    expect(piglatin("ugly")).to.equal("uglyay");
+    expect(piglatin("igloo")).to.equal("iglooay");
   });
 
+  it("will move all of the first consecutive consonants to the end and add 'ay' ", function() {
+    expect(piglatin("change")).to.equal("angechay");
+  });
 
 });
